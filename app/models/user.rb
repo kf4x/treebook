235f0fdfb389bf13d6f498afa5f569c2b3c678a4
Javier C <youracow@gmail.com>
2012-12-10 13:01:09 -0700

    ttd for profile_name fname lname

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,18 @@ class User < ActiveRecord::Base
   					:first_name, :last_name, :profile_name
   # attr_accessible :title, :body
 
+  validates_presence_of :first_name#, :message => "can't be blank"
+  validates_presence_of :last_name#, :message => "can't be blank"
+  validates_presence_of :profile_name#, :message => "can't be blank"
+
+  validates_uniqueness_of :profile_name
+
+  validates_format_of :profile_name, :with => /a-zA-Z\-\_+/, :message => "Must be formatted correctly."
+  # validates :first_name, presence: true
+  # validates :last_name, presence: true
+  # validates :profile_name, presence: true, uniqueness: true
+
+
   has_many :statuses
 
   def full_name
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,37 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	test "user should have a first name" do
+		user = User.new
+		assert !user.save
+		assert !user.errors[:first_name].empty?	#making sure the errors list is not empty for the first_name property
+	end
+
+	test "user should have last name" do
+		user = User.new
+		assert !user.save
+		assert !user.errors[:last_name].empty?
+	end
+	test "user should have profile name" do
+		user = User.new
+		assert !user.save
+		assert !user.errors[:profile_name].empty?
+	end
+
+	test "user should have unique profile name" do
+		user = User.new
+		user.profile_name = users(:javier).profile_name #getting profile name from fixture
+
+		assert !user.save
+		assert !user.errors[:profile_name].empty?		
+	end
+	test "user must format profile_name wO spaces" do
+		user = User.new 
+		user.profile_name = "Javier C"
+
+		assert !user.save							#should NOT save the user
+		assert !user.errors[:profile_name].empty?	#check errors array **should NOT be empty
+		assert user.errors[:profile_name].include?("Must be formatted correctly.") #this msg must be included
+	end
 end

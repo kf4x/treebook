7d939dd33ec7c458c5458d01a3a328894c86dd8b
Javier C <youracow@gmail.com>
2012-12-10 18:15:26 -0700

    styling to flash msg

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+	#this returns the CSS class name for noitice bar
+	def flash_type_hlpr (type)
+		case type
+		when :alert
+			"alert-error"	#class="alert alert-error"
+		when :notice
+			"alert-notice"	#class="alert alert-notice"
+		else
+			""
+		end
+	end
 end
